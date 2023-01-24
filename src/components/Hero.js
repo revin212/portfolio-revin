cee543c66135a40364f1778308b78d0cd8da99ae
Revin Dennis Ramadhan <revinramadhan@gmail.com>
2023-01-24 15:34:02 +0700

    navbar styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../images/revin-logo-transparan.png'
 
 export default function Hero() {
   return (
     <div className='hero-section'>
-        
+        <img src={logo} alt="" />
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import logo from '../images/revin-logo-transparan.png'
 
 export default function Navbar() {
   return (
-    <div className='flex justify-between'>
+    <div className='flex justify-between bg-main-black text-neutral-white'>
         <div className="leftmenu">
-            <Link to='/'>Revin Dennis R.</Link>
+            <Link to='/'>
+              <img src={logo} alt="" className='w-[50px]' />
+            </Link>
         </div>
         <div className="rightmenu">
             <Link to='/'>Skills</Link>
